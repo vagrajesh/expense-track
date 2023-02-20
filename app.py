d5263a3017215aa4114c3ce8e6d53c8d38a9f2f4
vagrajesh <97694832+vagrajesh@users.noreply.github.com>
2023-02-20 06:30:15 +0000

    added HTML tags and style

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
+
 app = Flask(__name__)
+
 
 @app.route("/")
 def helloworld():
-  return"Hello, Rajesh"
+  return render_template('home.html')
+
 
 print(__name__)
 
